5018748a5f5173a87ab762e132ed987b74a697fc
mbkhalid105-droid <mbkhalid105@gmail.com>
2025-10-31 09:11:19 +0000

    varaibls

---
+++ .devcontainer/01_ basics/ 01_variables.js
@@ -0,0 +1,19 @@
+const accountId =14453
+let accountEmail ="Hitesh@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bangaluru"
+let accountState;
+
+console.log(accountId);
+/*
+prefer not to used var
+beacuse of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatement])
